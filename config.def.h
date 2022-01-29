06669c62d845e288020b88e1e90cf1445bfd7cb5
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2022-01-28 22:08:47 -0300

    feat: added shortcut to slock
    Added a shortcut to lock the screen with slock.

--- config.def.h
+++ config.def.h
@@ -82,6 +82,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *screenlockcmd[]  = { "slock", NULL };
 
 /* ALSA volume controls */
 static const char *mutecmd[] =    { "amixer", "-q", "set", "Master", "toggle", NULL };
@@ -93,6 +94,7 @@ static Key keys[] = {
 	/* modifier                     key                       function        argument */
 	{ MODKEY,                       XK_p,                     spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return,                spawn,          {.v = termcmd } },
+	{ MODKEY,												XK_Escape,								spawn, 					{.v = screenlockcmd } },
 	{ MODKEY,                       XK_b,                     togglebar,      {0} },
 	{ MODKEY,                       XK_j,                     focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,                     focusstack,     {.i = -1 } },
