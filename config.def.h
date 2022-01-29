b13bc27e19b6f440cc0f6b3dfb6b296b45bf8a6f
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2022-01-29 19:13:29 -0300

    style: changing some config defaults
    Reduced external vertical gap.
    
    Added a fixed height to the top bar to increase it's size a bit.
    
    Disabled the shortcuts that move the focus togheter with the window when
    moving it between monitors. (it didn't fit the rest of the defaults and
    confused me a bit sometimes).

--- config.def.h
+++ config.def.h
@@ -8,11 +8,11 @@ static const int scalepreview       = 4;        /* tag preview scaling */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 22;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const int focusonwheel       = 0;
@@ -142,9 +142,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_comma,                 focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period,                focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,                 tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_comma,                 focusmon,       {.i = -1 } },
+	// { MODKEY|ShiftMask,             XK_comma,                 focusmon,       {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,                tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_period,                focusmon,       {.i = +1 } },
+	// { MODKEY|ShiftMask,             XK_period,                focusmon,       {.i = +1 } },
 	TAGKEYS(                        XK_1,                     0)
 	TAGKEYS(                        XK_2,                     1)
 	TAGKEYS(                        XK_3,                     2)
